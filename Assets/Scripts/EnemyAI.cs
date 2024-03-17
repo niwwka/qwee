5ccc48427bd66a51b39895f588b38f7e2fff48e4
niwwka <senya123op@yandex.ru>
2024-03-17 10:05:58 +0300

    3week

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -2,65 +2,71 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-
-
 public class EnemyAI : MonoBehaviour
 {
     public List<Transform> patrolPoints;
     public PlayerController player;
+    public float viewAngle;
 
     private NavMeshAgent _navMeshAgent;
     private bool _isPlayerNoticed;
 
-
-    // Start is called before the first frame update
     private void Start()
     {
         InitComponentLinks();
         PickNewPatrolPoint();
     }
-
-    // Update is called once per frame
     private void InitComponentLinks()
     {
         _navMeshAgent = GetComponent<NavMeshAgent>();
     }
-
     private void Update()
     {
-        var direcrion = player.transform.position - transform.position;
 
-        RaycastHit hit;
-        if(Physics.Raycast(transform.position + Vector3.up, direcrion, out hit) )
+        NoticePLayerUpdate();
+        if (_isPlayerNoticed )
         {
-            if(hit.collider.gameObject == player.gameObject)
-            {
-                _isPlayerNoticed = true;
-            }
-            else
-            {
-                _isPlayerNoticed = false;
-            }
+            _navMeshAgent.destination = player.transform.position;
         }
-        else
-        {
-            _isPlayerNoticed = false;
-        }    
-
+        ChaseUpdate();
 
         PatrolUpdate();
-    }    
+    }
+    private void NoticePLayerUpdate()
+    {
+        var direcrion = player.transform.position - transform.position;
+        _isPlayerNoticed = false;
+        if (Vector3.Angle(transform.forward, direcrion) < viewAngle)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position + Vector3.up, direcrion, out hit))
+            {
+                if (hit.collider.gameObject == player.gameObject)
+                {
+                    _isPlayerNoticed = true;
+                }
+            }
+        }
+    } 
     private void PatrolUpdate()
     {
-        if (_navMeshAgent.remainingDistance == 0)
+        if (!_isPlayerNoticed)
         {
-            PickNewPatrolPoint();
+            if (_navMeshAgent.remainingDistance == 0)
+            {
+                PickNewPatrolPoint();
+            }
         }
     }    
-    
-
     private void PickNewPatrolPoint()
     {
         _navMeshAgent.destination = patrolPoints[Random.Range(0, patrolPoints.Count)].position;
-    }    
+    }
+    private void ChaseUpdate()
+    {
+        if (_isPlayerNoticed)
+        {
+            _navMeshAgent.destination = player.transform.position;
+        }
+    }
 }
---
+++ Assets/Scripts/FireballSource.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireballSource : MonoBehaviour
+{
+    public Transform TargetPoint;
+    public Camera cameraLink;
+    public float targetInSkyDistance;
+
+
+    private void Update()
+    {
+        var ray = cameraLink.ViewportPointToRay(new Vector3(0.5f, 0.7f, 0));
+
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            TargetPoint.position = hit.point;
+        }
+        else
+        {
+            TargetPoint.position = ray.GetPoint(targetInSkyDistance);
+        }
+
+        transform.LookAt(TargetPoint.position);
+    }
+}
